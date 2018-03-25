33ea9b8dcea1610fe0ea32966e415bc946448d7d
Emily <emily.s.gao@gmail.com>
2018-03-25 17:04:52 -0600

    modify production ENV

--- knexfile.js
+++ knexfile.js
@@ -21,16 +21,35 @@ module.exports = {
     }
   },
 
+  // production: {
+  //   client: 'postgresql',
+  //   connection: process.env.DATABASE_URL + '?ssl=true',
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'migrations'
+  //   }
+  // }
+
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
+    connection: {
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
+      ssl      : process.env.DB_SSL
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
-  }
+  },
 
 };

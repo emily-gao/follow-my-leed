8256430543a12d2f3134dafcc9b2de5c0518bf38
Emily <emily.s.gao@gmail.com>
2018-03-25 13:35:03 -0600

    merge in branch 'feature/deployment'

--- start-react-server.js
+++
@@ -1,4 +0,0 @@
-/** Runs 'npm start' in the client directory */
-const args = ['start'];
-const opts = { stdio: 'inherit', cwd: 'react-server', shell: true };
-require('child_process').spawn('npm', args, opts);
